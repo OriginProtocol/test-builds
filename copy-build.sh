2d033fbecf2838a402956be9fdf4979b3ad2d27f
Nick Poulden <nick@poulden.com>
2019-02-10 13:17:00 -0700

    nick

--- copy-build.sh
+++ copy-build.sh
@@ -2,8 +2,12 @@
 
 if [ "$1" == "" ]; then
     echo "Usage: ./copy-build [dir]"
+    exit 1
 fi
 
 mkdir -p $1
 rm $1/app.* || true
 cp -r ../origin/experimental/origin-dapp2/public/* $1
+git add .
+git commit -m $1
+git push
