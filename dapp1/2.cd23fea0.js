deb8bd9b0bd6f75c6708842cd4065898a8a10390
Tom Linton <tom@originprotocol.com>
2019-03-07 15:35:49 +1300

    Add dapp1

---
+++ dapp1/2.cd23fea0.js
@@ -0,0 +1,7 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{3014:
+/*!*******************************************!*\
+  !*** ./locale-data/complete.js (ignored) ***!
+  \*******************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(n,w){}}]);
