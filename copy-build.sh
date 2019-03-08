0400caf8e6290cf79e5df07242df626f5624d991
Nick Poulden <nick@poulden.com>
2019-03-08 09:22:00 -0700

    nick

--- copy-build.sh
+++ copy-build.sh
@@ -8,7 +8,7 @@ fi
 git pull --rebase
 mkdir -p $1
 rm $1/app.* || true
-cp -r ../origin/experimental/origin-dapp2/public/* $1
+cp -r ../origin/origin-dapp/public/* $1
 git add .
 git commit -m $1
 git push
